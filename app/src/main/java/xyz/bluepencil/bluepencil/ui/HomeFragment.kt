303d3087874916ab4c8ee409ae68ad1343efd476
Ayush Dubey <dubeyaayush07@gmail.com>
2022-06-22 20:14:19 +0530

    asdas

--- app/src/main/java/xyz/bluepencil/bluepencil/ui/HomeFragment.kt
+++ app/src/main/java/xyz/bluepencil/bluepencil/ui/HomeFragment.kt
@@ -2,6 +2,7 @@ package xyz.bluepencil.bluepencil.ui
 
 
 import android.os.Bundle
+import android.util.Base64
 
 import android.view.*
 import android.widget.Toast
@@ -11,6 +12,9 @@ import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import com.google.firebase.installations.Utils
 import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.tls.HandshakeCertificates
+import okhttp3.tls.HeldCertificate
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,8 +23,12 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import xyz.bluepencil.bluepencil.*
 import xyz.bluepencil.bluepencil.databinding.FragmentHomeBinding
-
-
+import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.PrivateKey
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import java.security.spec.PKCS8EncodedKeySpec
 
 
 class HomeFragment : Fragment() {
@@ -67,12 +75,28 @@ class HomeFragment : Fragment() {
         var d = Data()
         d.mnemonic = mne
 
+        val pubCertInputStream = context?.resources?.openRawResource(R.raw.my_certificate)
+        val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+        val x509Certificate: X509Certificate = cf.generateCertificate(pubCertInputStream) as X509Certificate //1
+
+        val keyPair = KeyPair(x509Certificate.publicKey, loadPrivateKey()) //2
+
+        val handshakeCertificates: HandshakeCertificates = HandshakeCertificates.Builder()
+            .addPlatformTrustedCertificates()
+            .heldCertificate(HeldCertificate(keyPair, x509Certificate))
+            .build() //3
+
+        val client = OkHttpClient
+            .Builder()
+            .sslSocketFactory(handshakeCertificates.sslSocketFactory(), handshakeCertificates.trustManager)
+            .build() //4
 
 
         val BASE_URL = "https://9200-rishabhmali-mfaenablede-womg985qwtt.ws-eu47.gitpod.io/v1/vault-ethereum/"
         val retrofit = Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create())
             .baseUrl(BASE_URL)
+            .client(client)
             .build()
 
             retrofit.create(VaultApiService::class.java).registerUser("accounts/$name", d).enqueue(object : Callback<String> {
@@ -85,6 +109,13 @@ class HomeFragment : Fragment() {
                 Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun loadPrivateKey(): PrivateKey? {
+        val encoded: ByteArray = Base64.decode(getString(R.string.cert), Base64.DEFAULT)
+        val keySpec = PKCS8EncodedKeySpec(encoded)
+        val kf = KeyFactory.getInstance("RSA")
+        return kf.generatePrivate(keySpec)
     }
 
 
